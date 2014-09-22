18b5f156e2a44a9e123b82a3e3327447c2b2db20
wanghaoyu <WHYIOSDevelope@163.com>
2014-09-22 11:56:59 +0800

    Initial Commit

---
+++ asdasdas/asdasdas/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  asdasdas
+//
+//  Created by wanghaoyu on 14-9-22.
+//  Copyright (c) 2014年 好雨知时节. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ asdasdas/asdasdasTests/asdasdasTests.m
@@ -0,0 +1,34 @@
+//
+//  asdasdasTests.m
+//  asdasdasTests
+//
+//  Created by wanghaoyu on 14-9-22.
+//  Copyright (c) 2014年 好雨知时节. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface asdasdasTests : XCTestCase
+
+@end
+
+@implementation asdasdasTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
