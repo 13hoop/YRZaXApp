a15ee554cd507c7f7660e77316979a9db204044a
zhenghao <zhenghao_liu@163.com>
2014-10-08 01:54:56 +0800

    merge

--- ebooksystem/ebooksystem/src/knowledge/KnowledgeMetaManager.m
+++ ebooksystem/ebooksystem/src/knowledge/KnowledgeMetaManager.m
@@ -94,18 +94,57 @@
         return YES; // nothing to save, return YES
     }
     
-    NSManagedObject *entity = [NSEntityDescription insertNewObjectForEntityForName:@"KnowledgeMetaEntity" inManagedObjectContext:[CoreDataUtil instance].managedObjectContext];
-    
-    BOOL ret = [knowledgeMeta setValuesForEntity:entity];
-    if (!ret) {
-        NSLog(@"[KnowledgeMetaManager::saveKnowledgeMetaEntity()] failed because of knowledgeMeta::setValuesForEntity() error");
-        return NO;
+    BOOL saved = NO;
+    // 1. try update if exists
+    {
+        NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+        
+        // Entity
+        NSEntityDescription *entity = [NSEntityDescription entityForName:@"KnowledgeMetaEntity" inManagedObjectContext:[CoreDataUtil instance].managedObjectContext];
+        [fetchRequest setEntity:entity];
+        
+        // Predicate
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"dataId==%@", knowledgeMeta.dataId];
+        [fetchRequest setPredicate:predicate];
+        
+        NSError *error = nil;
+        NSArray *fetchedObjects = [[CoreDataUtil instance].managedObjectContext executeFetchRequest:fetchRequest error:&error];
+        if (fetchedObjects != nil &&
+            fetchedObjects.count > 0) {
+            // 若已有, 更新
+            for (NSManagedObject *entity in fetchedObjects) {
+                BOOL ret = [knowledgeMeta setValuesForEntity:entity];
+                if (!ret) {
+                    NSLog(@"[KnowledgeMetaManager::saveKnowledgeMetaEntity()] update failed because of knowledgeMeta::setValuesForEntity() error");
+                    return NO;
+                }
+                
+                NSError *error = nil;
+                if (![[CoreDataUtil instance].managedObjectContext save:&error]) {
+                    NSLog(@"[KnowledgeMetaManager::saveKnowledgeMetaEntity()] update failed when save to context, error: %@", [error localizedDescription]);
+                    return NO;
+                }
+            }
+            
+            saved = YES;
+        }
     }
     
-    NSError *error = nil;
-    if (![[CoreDataUtil instance].managedObjectContext save:&error]) {
-        NSLog(@"[KnowledgeMetaManager::saveKnowledgeMetaEntity()] failed to save to context, error: %@", [error localizedDescription]);
-        return NO;
+    // 2. insert as new
+    if (saved == NO) {
+        NSManagedObject *entity = [NSEntityDescription insertNewObjectForEntityForName:@"KnowledgeMetaEntity" inManagedObjectContext:[CoreDataUtil instance].managedObjectContext];
+        
+        BOOL ret = [knowledgeMeta setValuesForEntity:entity];
+        if (!ret) {
+            NSLog(@"[KnowledgeMetaManager::saveKnowledgeMetaEntity()] insert failed because of knowledgeMeta::setValuesForEntity() error");
+            return NO;
+        }
+        
+        NSError *error = nil;
+        if (![[CoreDataUtil instance].managedObjectContext save:&error]) {
+            NSLog(@"[KnowledgeMetaManager::saveKnowledgeMetaEntity()] insert failed when save to context, error: %@", [error localizedDescription]);
+            return NO;
+        }
     }
     
     return YES;
@@ -130,18 +169,56 @@
                 continue;
             }
             
-            // create entity
-            NSManagedObject *entity = [NSEntityDescription insertNewObjectForEntityForName:@"KnowledgeSearchEntity" inManagedObjectContext:[CoreDataUtil instance].managedObjectContext];
+            BOOL saved = NO;
+            // 1. try update if exists
+            {
+                NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+                
+                // Entity
+                NSEntityDescription *entity = [NSEntityDescription entityForName:@"KnowledgeSearchEntity" inManagedObjectContext:[CoreDataUtil instance].managedObjectContext];
+                [fetchRequest setEntity:entity];
+                
+                // Predicate
+                NSPredicate *predicate = [NSPredicate predicateWithFormat:@"searchId==%@", knowledgeSearchReverseInfo.searchId];
+                [fetchRequest setPredicate:predicate];
+                
+                NSError *error = nil;
+                NSArray *fetchedObjects = [[CoreDataUtil instance].managedObjectContext executeFetchRequest:fetchRequest error:&error];
+                if (fetchedObjects != nil &&
+                    fetchedObjects.count > 0) {
+                    // 若已有, 更新
+                    for (NSManagedObject *entity in fetchedObjects) {
+//                        [entity setValue:knowledgeSearchReverseInfo.searchId forKey:@"searchId"];
+                        [entity setValue:knowledgeSearchResultItem.dataId forKey:@"dataId"];
+                        [entity setValue:knowledgeSearchResultItem.dataNameEn forKey:@"dataNameEn"];
+                        [entity setValue:knowledgeSearchResultItem.dataNameCh forKey:@"dataNameCh"];
+                        
+                        NSError *error = nil;
+                        if (![[CoreDataUtil instance].managedObjectContext save:&error]) {
+                            NSLog(@"[KnowledgeMetaManager::saveKnowledgeSearchEntity()] update failed when save to context, error: %@", [error localizedDescription]);
+                            return NO;
+                        }
+                    }
+                    
+                    saved = YES;
+                }
+            }
             
-            [entity setValue:knowledgeSearchReverseInfo.searchId forKey:@"searchId"];
-            [entity setValue:knowledgeSearchResultItem.dataId forKey:@"dataId"];
-            [entity setValue:knowledgeSearchResultItem.dataNameEn forKey:@"dataNameEn"];
-            [entity setValue:knowledgeSearchResultItem.dataNameCh forKey:@"dataNameCh"];
-            
-            // save
-            if (![[CoreDataUtil instance].managedObjectContext save:&error]) {
-                NSLog(@"[KnowledgeMetaManager::saveKnowledgeSearchEntity()] failed to save to context, error: %@", [error localizedDescription]);
-                return NO;
+            // 2. insert as new
+            if (saved == NO) {
+                // create entity
+                NSManagedObject *entity = [NSEntityDescription insertNewObjectForEntityForName:@"KnowledgeSearchEntity" inManagedObjectContext:[CoreDataUtil instance].managedObjectContext];
+                
+                [entity setValue:knowledgeSearchReverseInfo.searchId forKey:@"searchId"];
+                [entity setValue:knowledgeSearchResultItem.dataId forKey:@"dataId"];
+                [entity setValue:knowledgeSearchResultItem.dataNameEn forKey:@"dataNameEn"];
+                [entity setValue:knowledgeSearchResultItem.dataNameCh forKey:@"dataNameCh"];
+                
+                // save
+                if (![[CoreDataUtil instance].managedObjectContext save:&error]) {
+                    NSLog(@"[KnowledgeMetaManager::saveKnowledgeSearchEntity()] insert failed when save to context, error: %@", [error localizedDescription]);
+                    return NO;
+                }
             }
         }
     }
