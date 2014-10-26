a99cc7936a15846528ce6ba9230414f8b3e54045
Jess <maochengjie1988@gmail.com>
2014-10-26 16:53:44 +0800

    debug back click

--- ebooksystem/ebooksystem/assets/knowledge_data/politics_dagangbianhuakaodian/20/10255d7bc018932ce1910a92d585a114/utils.js
+++ ebooksystem/ebooksystem/assets/knowledge_data/politics_dagangbianhuakaodian/20/10255d7bc018932ce1910a92d585a114/utils.js
@@ -60,7 +60,8 @@ utils.initPageHeader = function( selector ){
 
     var commonBack = document.querySelector( selector );
     if( commonBack ){
-        commonBack.addEventListener( 'touchend', function(){
+        alert('common back inited' );
+        commonBack.addEventListener( 'touchend', function(){                                    
             bridgeIOS.finish();
         }, false );
     }
