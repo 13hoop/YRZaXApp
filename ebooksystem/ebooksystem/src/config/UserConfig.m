38a256843bda3c16960308f96d66360f7f8111ab
zhenghao <zhenghao_liu@163.com>
2014-11-26 15:43:19 +0800

    再次更新用户信息页url

--- ebooksystem/ebooksystem/src/config/UserConfig.m
+++ ebooksystem/ebooksystem/src/config/UserConfig.m
@@ -21,7 +21,8 @@
 
 // 用户信息url
 - (NSString *)urlForUserInfo {
-    return @"http://test.zaxue100.com/index.php?c=passportctrl&m=show_userinfo_page";
+//    return @"http://test.zaxue100.com/index.php?c=passportctrl&m=show_userinfo_page";
+    return @"http://test.zaxue100.com/index.php?c=passportctrl&m=show_userinfo_page&srcurl=http%3A%2F%2Ftest.zaxue100.com%2Findex.php%3Fc%3Dpassportctrl%26m%3Dshow_userinfo_page%26back_to_app%3D1";
 }
 
 // 登入url
